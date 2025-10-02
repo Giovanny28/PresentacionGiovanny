cf10e5dd14d16b67bdd83b3c4aecd1bb4af80536
Giovanny28 <giovannypn28@gmail.com>
2025-10-01 20:48:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function verificarPassword() {
+  const claveCorrecta = "12345"; // 🔑 Aquí defines tu contraseña
+  const input = document.getElementById("password").value;
+  const mensaje = document.getElementById("mensaje");
 
+  if (input === claveCorrecta) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("contenido").style.display = "block";
+  } else {
+    mensaje.textContent = "❌ Contraseña incorrecta. Intenta de nuevo.";
+  }
+}
